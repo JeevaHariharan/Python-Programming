00f74038e4e5d09a2de76da92a90cc2e24c836fb
JeevaHariharan <37337178+JeevaHariharan@users.noreply.github.com>
2018-03-18 14:33:54 +0530

    Create Alphabet or not.c

---
+++ Beginner Level/Alphabet or not.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char character;
+scanf("%c",&character);
+if(character>='a')
+{
+printf("Alphabet\n");
+}
+else if(character>='A')
+{
+printf("Alphabet\n");
+}
+else
+{
+printf("No");
+}
+}
